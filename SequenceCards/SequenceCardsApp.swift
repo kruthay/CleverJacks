569a95870a11e0e7824056c0ecb4d8f429c17940
Kruthay Donapati <kruthay.vfn105@gmail.com>
2023-07-03 15:03:08 -0400

    Initial Commit

---
+++ SequenceCards/SequenceCardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SequenceCardsApp.swift
+//  SequenceCards
+//
+//  Created by Kruthay Kumar Reddy Donapati on 7/3/23.
+//
+
+import SwiftUI
+
+@main
+struct SequenceCardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
