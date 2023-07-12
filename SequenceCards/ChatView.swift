aa7c406ee0c3c6cb9b11e23042424ce8fb2d540b
Kruthay Donapati <kruthay.vfn105@gmail.com>
2023-07-11 23:39:44 -0400

    Chat and Reminder functionality with System sounds support is added

---
+++ SequenceCards/ChatView.swift
@@ -0,0 +1,18 @@
+//
+//  ChatView.swift
+//  CleverJacks
+//
+//  Created by Kruthay Kumar Reddy Donapati on 7/11/23.
+//
+
+import SwiftUI
+
+struct ChatView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ChatView()
+}
---
+++ SequenceCards/MessageView.swift
@@ -0,0 +1,18 @@
+//
+//  MessageView.swift
+//  CleverJacks
+//
+//  Created by Kruthay Kumar Reddy Donapati on 7/11/23.
+//
+
+import SwiftUI
+
+struct MessageView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MessageView()
+}
